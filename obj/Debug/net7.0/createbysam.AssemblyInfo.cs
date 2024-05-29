c867bb241fbf5ed55371b76412cfbe2902d68c92
Azohajutt <azoha6966@gmail.com>
2024-05-29 15:58:14 +0500

    zohachanges

--- obj/Debug/net7.0/createbysam.AssemblyInfo.cs
+++ obj/Debug/net7.0/createbysam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("createbysam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d57c21e2015815cd8eac2c1950d15af7b50a0d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("createbysam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("createbysam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
